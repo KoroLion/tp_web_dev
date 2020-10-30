b8f0228c692246476c16f280c8cea9889654750e
KoroLion <korolion31@yandex.ru>
2020-10-30 14:11:26 +0300

    Signup, login, header and footer templates

--- src/lk_moments/urls.py
+++ src/lk_moments/urls.py
@@ -5,7 +5,7 @@ from registration.views import RegistrationView
 from authentication.views import LoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signup/', RegistrationView.as_view()),
-    path('login/', LoginView.as_view()),
+    path('admin/', admin.site.urls, name="admin"),
+    path('signup/', RegistrationView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(), name="login"),
 ]
